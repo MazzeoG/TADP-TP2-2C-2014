57ed31053c876e2d21556839c92573e630372d18
FranciscoPinieiro <panchopr_91@hotmail.com>
2014-11-09 10:24:33 -0300

    Fix de Casa Central

--- src/tadp/tp/argentinaexpress/CasaCentral.scala
+++ src/tadp/tp/argentinaexpress/CasaCentral.scala
@@ -2,7 +2,7 @@ package tadp.tp.argentinaexpress
 
 import scala.collection.immutable.Set
 
-class CasaCentral(override val transporte : Set[Transporte],override val volumenTotal : Int, override val Pais: String) 
+class CasaCentral(transporte : Set[Transporte],override val volumenTotal : Int, override val Pais: String) 
 extends Sucursal(transporte,volumenTotal,Pais) {
 
 }
